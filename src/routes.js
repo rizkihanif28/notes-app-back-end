8dead8e7f674e612db7887d9431250e1663ab443
rizkihnf <rizkihnf2@gmail.com>
2023-05-12 11:24:19 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addNoteHandler, getAllNotesHandler, GetNotByIdHandler, editNoteByIdHandler, deletNoteByIdHandler } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: GetNotByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deletNoteByIdHandler,
+    },
+];
+
+module.exports = routes;
